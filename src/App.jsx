6c8ccb4019fe05ed0293e4fe584661cec498efb3
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-10-08 22:39:01 +0200

    started with project-music-release-vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import data from "./data.json";
+import { Header } from "./components/Header.jsx"
 
 console.log(data);
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <>
+      <Header />
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <p>Hello hello</p>
+    </>
+
+  );
 };
